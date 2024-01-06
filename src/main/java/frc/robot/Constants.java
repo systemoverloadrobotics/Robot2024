6fc3e9838d9205c6a2678848894ced3978881834
OneLemonyBoi <onelemonyboii@gmail.com>
2024-01-06 00:04:05 -0800

    Janky Code to Start the Year :heart:

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,16 +4,118 @@
 
 package frc.robot;
 
+
+import java.io.IOException;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import frc.sorutil.motor.PidProfile;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes) wherever the constants are needed, to
+ * reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    public static final String PROJECT_NAME = "Robot2024";
+
+    // Periodic timing of the robot, WPILib default is 0.02 (20ms)
+    public static final Double ROBOT_PERIOD = 0.02; // 20 ms
+
+    // Configure the power module used on the robot
+    public static final ModuleType POWER_MODULE_TYPE = ModuleType.kRev;
+
+    public static final double NOMINAL_VOLTAGE = 12.0;
+
+    public static final class RobotDimensions {
+        // TODO: replace these with actual dimensions
+        public static final double WIDTH = Units.inchesToMeters(30);
+        public static final double LENGTH = Units.inchesToMeters(30);
+
+        public static final double WHEEL_CIRCUMFERENCE = Units.inchesToMeters(4) * Math.PI;
+        public static final SwerveDriveKinematics SWERVE_DRIVE_KINEMATICS =
+                new SwerveDriveKinematics(new Translation2d(RobotDimensions.LENGTH / 2, -RobotDimensions.WIDTH / 2),
+                        new Translation2d(RobotDimensions.LENGTH / 2, RobotDimensions.WIDTH / 2),
+                        new Translation2d(-RobotDimensions.LENGTH / 2, -RobotDimensions.WIDTH / 2),
+                        new Translation2d(-RobotDimensions.LENGTH / 2, RobotDimensions.WIDTH / 2));
+    }
+
+    public static final class Vision {
+        // Camera location from the center of the robot
+        public static final double CAMERA_POSITION_X = 0; // Meters
+        public static final double CAMERA_POSITION_Y = 0.5; // Meters
+        public static final double CAMERA_POSITION_Z = 0; // Meters
+        public static final double CAMERA_ROTATION_ROLL = 0; // Radians
+        public static final double CAMERA_ROTATION_PITCH = 0; // Radians
+        public static final double CAMERA_ROTATION_YAW = 0; // Radians
+
+        public static final Translation3d CAMERA_POSITION =
+                new Translation3d(CAMERA_POSITION_X, CAMERA_POSITION_Y, CAMERA_POSITION_Z);
+        public static final Rotation3d CAMERA_ROTATION =
+                new Rotation3d(CAMERA_ROTATION_ROLL, CAMERA_ROTATION_PITCH, CAMERA_ROTATION_YAW);
+
+        public static final AprilTagFieldLayout TAG_FIELD_LAYOUT;
+
+        static {
+            AprilTagFieldLayout temp = null;
+            try {
+                temp = AprilTagFieldLayout.loadFromResource("/edu/wpi/first/apriltag/2023-chargedup.json");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            TAG_FIELD_LAYOUT = temp;
+        }
+    }
+
+    public static final class Motor {
+        // Motor indexes + configs here
+    }
+
+    public static final class Pneumatics {
+        
+    }
+
+    public static final class Swerve {
+        // SET THESE UP! TODO
+        public static final PidProfile STEER_PROFILE = new PidProfile(0, 0, 0.);
+        public static final PidProfile POWER_PROFILE = new PidProfile(0, 0, 0);
+
+        public static final double SWERVE_POWER_CURRENT_LIMIT = 60.0;
+        public static final double SWERVE_POWER_MAX_OUTPUT = 0.8;
+
+        public static final double SWERVE_ROTATION_CURRENT_LIMIT = 40.0;
+        public static final double SWERVE_ROTATION_MAX_OUTPUT = 0.7;
+
+        public static final double DISTANCE_PER_REV = Units.inchesToMeters(4 * Math.PI);
+        public static final double NEO_MAX_SPEED = 5600; // RPM
+        public static final double MAX_WHEEL_SPEED = ((NEO_MAX_SPEED / 60) * DISTANCE_PER_REV);
+        public static final double SWERVE_MAX_SPEED = 6; // m/s
+        public static final double SWERVE_MAX_AUTO_SPEED = 0.2 * MAX_WHEEL_SPEED; // m/s
+        public static final double SWERVE_MAX_PRECISION_SPEED = 0.1 * MAX_WHEEL_SPEED; // m/s
+        public static final double SWERVE_MAX_ACCELERATION = 2; // m/s^2
+        public static final double SWERVE_ROTATION_MAX_SPEED = Math.PI * 2; // rad/s
+        public static final double SWERVE_ROTATION_MAX_ACCELERATION = Math.PI * 2 / 3; // rads/s^2
+
+        public static final double SWERVE_DEADBAND = 0.05;
+        public static final double SWERVE_ROTATION_TOLERANCE = 5; // degrees
+        public static final double SWERVE_SNAPPING_DEADBAND = 0.5; // 50%
+    }
+
+    public static final class Input {
+        
+    }
+    public static final class Auto {
+        
+    }
+    public static final class PoseEstimation {
+        
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -21,10 +20,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -43,12 +38,11 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    new Trigger(m_exampleSubsystem::exampleCondition).onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**

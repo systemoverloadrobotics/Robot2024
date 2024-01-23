224261427daea3f87affbd5b35dc26d3246ce1d0
OneLemonyBoi <onelemonyboii@gmail.com>
2024-01-22 16:12:13 -0800

    Setup AUTO

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,27 +4,45 @@
 
 package frc.robot;
 
+import frc.robot.Constants.Swerve;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Swerve swerve;
+  private final SendableChooser<Command> autoSelector;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    swerve = new Swerve();
     // Configure the trigger bindings
     configureBindings();
+    autoSelector = AutoBuilder.buildAutoChooser();
+    configureAuto();
+  }
+
+  private void configureAuto() {
+    // NamedCommands.registerCommand("AutoMoveToIntake", [ADD COMMAND]);
+    // NamedCommands.registerCommand("AutoIntake", [ADD COMMAND]);
+    // NamedCommands.registerCommand("AutoMoveToShoot", [ADD COMMAND]);
+    // NamedCommands.registerCommand("AutoShoot", [ADD COMMAND]);
+
+    autoSelector.addOption("BM2", AutoBuilder.followPath(PathPlannerPath.fromPathFile("BM2")));
+    autoSelector.addOption("BM3", AutoBuilder.followPath(PathPlannerPath.fromPathFile("BM3")));
+    SmartDashboard.putData("Auto Selector", autoSelector);
   }
 
   /**
@@ -37,21 +55,13 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition).onTrue(new ExampleCommand(m_exampleSubsystem));
+    
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
     // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return autoSelector.getSelected();
   }
 }
